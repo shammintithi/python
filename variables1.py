26e1f4138849ae17c14e7b4ae318d7d4587a4f82
shammintithi <tanhashammintithi@gmail.com>
2025-11-08 01:56:11 +0600

    python basic 1 to 5

---
+++ variables1.py
@@ -0,0 +1,16 @@
+name = "tithi"
+age = 25
+height = 5.6
+is_student = True
+favorite_subjects = ["Math", "Science", "Art"]
+address = {
+    "street": "123 Main St",
+    "city": "Anytown",
+    "zip": "12345"
+}
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+print("Favorite Subjects:", favorite_subjects)
+print("Address:", address)
