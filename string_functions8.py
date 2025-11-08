0c118ca2bdb9d73c19a710cbb3077c46b62d60cf
shammintithi <tanhashammintithi@gmail.com>
2025-11-09 01:36:09 +0600

    python basic 6 to 10: input, substring, slicing, length, string function and conditional statements

--- string_functions8.py
+++ string_functions8.py
@@ -4,7 +4,7 @@ print(str.endswith("us")) #retuns true if yes
 str1 = "allah"
 print(str1.capitalize()) #capitalize the first letter.
 
-str2 = "Program language python"
+str2 = "Prog ram language python"
 print(str2.replace("g", "N"))
 print(str2.replace("python", "Java"))
 
@@ -15,4 +15,6 @@ print(str2.find("v"))
 
 str3 = "Program language python"
 print(str3.count("g"))
+
+print(len(str3)) #length of string
 
